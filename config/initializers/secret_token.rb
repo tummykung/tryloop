302a3278e3c7b0cc5ae1990be30c86e67fac357a
Tum Chaturapruek <tummykung@gmail.com>
2013-06-15 14:10:04 -0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryLoop::Application.config.secret_token = '8687c402fc3b11ac232bafcc76548d3ce1b6273f225eb2c9399dc754ae09263635701930723e7be28020f95feddc023e777627563088bc35bdc42356618ff4de'
